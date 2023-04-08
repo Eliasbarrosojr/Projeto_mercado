882719115589e9114fbd66a2c8446fdff35d5a70
Elias Jr <eliasbarroso222@gmail.com>
2023-04-08 10:56:19 -0300

    feat: create interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,1 +1,33 @@
-const product = [];
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  weight: number;
+  calories?: number;
+  section: "food" | "cleaning";
+  expirationDate: Date;
+}
+
+interface IProductsBase {
+  total: number;
+  marketProducts: IProduct[];
+}
+
+type ICleaningProduct = IProduct;
+
+interface IFoodProduct extends IProduct {
+  calories: number;
+}
+
+type IProductRequest = Omit<IProduct, "id" | "expirationDate">;
+
+type IMarket = IProduct[];
+
+export {
+  IProduct,
+  ICleaningProduct,
+  IFoodProduct,
+  IProductRequest,
+  IProductsBase,
+  IMarket,
+};
