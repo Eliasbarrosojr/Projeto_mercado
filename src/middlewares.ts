512ca0f896881f6319f1671b171f01bfd6b4a4e1
Elias Jr <eliasbarroso222@gmail.com>
2023-04-08 11:00:25 -0300

    feat: create middlewares

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,1 +1,54 @@
-const product5 = [];
+import { NextFunction, Request, Response } from "express";
+import { IProductRequest } from "./interfaces";
+import { marketProd } from "./database";
+
+const nameExist = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const name = request.body;
+
+  if (Array.isArray(name)) {
+    name.forEach((product: IProductRequest) => {
+      marketProd.forEach((element) => {
+        if (product.name == element.name) {
+          return response.status(409).json({
+            error: "Product already registered",
+          });
+        }
+      });
+    });
+  } else {
+    marketProd.forEach((element) => {
+      if (name.name == element.name) {
+        return response.status(409).json({
+          error: "Product already registered",
+        });
+      }
+    });
+  }
+
+  next();
+};
+
+const idExist = (request: Request, response: Response, next: NextFunction) => {
+  const id = Number(request.params.id);
+
+  const searchId = marketProd.findIndex((prod) => prod.id === id);
+
+  if (searchId === -1) {
+    return response.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  response.locals.id = {
+    idProduct: id,
+    searchedId: searchId,
+  };
+
+  next();
+};
+
+export { nameExist, idExist };
