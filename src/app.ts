aa42b0e742456a711d46a9371b9da4b4d0468370
Elias Jr <eliasbarroso222@gmail.com>
2023-04-08 11:02:55 -0300

    fix: uncomment imports de aap.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
 import express, { Application, json } from "express";
-/* import {
+import {
   createProduct,
   deleteProduct,
   searchAllProducts,
   searchById,
   Update,
 } from "./logics";
-import { idExist, nameExist } from "./Middlewares"; */
+import { idExist, nameExist } from "./middlewares";
 
 const app: Application = express();
 app.use(json());
 
-/* app.post("/products", nameExist, createProduct);
+app.post("/products", nameExist, createProduct);
 app.get("/products", searchAllProducts);
 app.get("/products/:id", idExist, searchById);
 app.patch("/products/:id", idExist, nameExist, Update);
-app.delete("/products/:id", idExist, deleteProduct); */
+app.delete("/products/:id", idExist, deleteProduct);
 
 app.listen(3000, () => {
   console.log("Server is running");
