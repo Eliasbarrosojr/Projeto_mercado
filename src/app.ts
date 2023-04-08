8be0a2c1b68aa4cebffb2c5bb7f520f494b6d3a9
Elias Jr <eliasbarroso222@gmail.com>
2023-04-08 10:53:22 -0300

    feat: create files ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-const product = [];
+import express, { Application, json } from "express";
+/* import {
+  createProduct,
+  deleteProduct,
+  searchAllProducts,
+  searchById,
+  Update,
+} from "./logics";
+import { idExist, nameExist } from "./Middlewares"; */
+
+const app: Application = express();
+app.use(json());
+
+/* app.post("/products", nameExist, createProduct);
+app.get("/products", searchAllProducts);
+app.get("/products/:id", idExist, searchById);
+app.patch("/products/:id", idExist, nameExist, Update);
+app.delete("/products/:id", idExist, deleteProduct); */
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
---
+++ src/database.ts
@@ -0,0 +1,6 @@
+/* import { IProduct, IProductsBase } from "./interfaces";
+
+const marketProd: Array<IProduct> = [];
+const Products: Array<IProductsBase> = [];
+
+export { marketProd, Products }; */
